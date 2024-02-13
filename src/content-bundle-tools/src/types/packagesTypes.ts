a5f88a11f140f03986395b906beae009992c6b49
Serhii Borshchov <s.borshchov.dev@gmail.com>
2024-02-13 15:10:09 +0200

    [content-bundle-tool] Minor fixes

--- src/content-bundle-tools/src/types/packagesTypes.ts
+++ src/content-bundle-tools/src/types/packagesTypes.ts
@@ -25,19 +25,6 @@ export interface PackageDetailsDto extends PackageDto {
   total_bundles: bigint
 }
 
-/*
-{ created=1707225273809262881;
-creator={identity_type=variant {ICP}; identity_id="br5f7-7uaaa-aaaaa-qaaca-cai"};
-owner= {identity_type=variant {ICP}; identity_id="nko75-gurhe-pmt5b-mhzyt-42njv-cdchk-fnwyz-ye6k6-melkn-btfz2-tae"};
-name="Amazing places in the Middle East";
-description="Most beautiful cities and locations in the Middle East";
-logo_url=opt "http://c2lt4-zmaaa-aaaaa-qaaiq-cai.localhost:4943/r/logo";
-max_supply=opt 200;
-total_bundles=10;
-submission=variant {Public}}
-*
-* */
-
 export interface DeployPackageParams {
   name: string
   description: string
