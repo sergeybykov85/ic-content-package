7cf1ccc392054f0ddb7c5674a50d97a367d76778
Serhii Borshchov <s.borshchov.dev@gmail.com>
2024-02-21 16:23:24 +0200

    [content-bundle-tool] POI item Location item

--- src/content-bundle-tools/src/components/features/GoogleMap/GoogleMap.tsx
+++ src/content-bundle-tools/src/components/features/GoogleMap/GoogleMap.tsx
@@ -1,4 +1,5 @@
 import type { FC } from 'react'
+import styles from './GoogleMap.module.scss'
 
 const API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
 
@@ -12,6 +13,9 @@ interface GoogleMapProps {
 
 const GoogleMap: FC<GoogleMapProps> = ({ location, height = 450 }) => {
   const { latitude, longitude } = location
+  if (!API_KEY) {
+    return <div className={styles['no-api-key']}>Please, add Google API key to render a map</div>
+  }
   return (
     <iframe
       width="100%"
