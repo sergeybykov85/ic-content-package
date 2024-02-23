fa3e2c6b059c53b7d25d0db755795514ab2261de
Serhii Borshchov <s.borshchov.dev@gmail.com>
2024-02-23 18:17:35 +0200

    [content-bundle-tool] Bundles back to list fix

--- src/content-bundle-tools/src/router/pages/BundleDetailsPage.tsx
+++ src/content-bundle-tools/src/router/pages/BundleDetailsPage.tsx
@@ -1,18 +1,17 @@
 import { type FC, useCallback, useMemo } from 'react'
 import { Navigate, useLocation, useNavigate, useParams } from 'react-router-dom'
 import SectionLayout from '~/components/layouts/SectionLayout'
-import If from '~/components/general/If'
 import Button from '~/components/general/Button'
-import { useAuth } from '~/context/AuthContext'
 import BundleDetailsBlock from '~/components/features/BundleDetailsBlock'
 import CopyBtn from '~/components/general/CopyBtn'
 
 const BundleDetailsPage: FC = () => {
   const { bundleId = '', packageId } = useParams()
-  const { isAuthenticated } = useAuth()
   const { state } = useLocation()
   const navigate = useNavigate()
-  const goBack = useCallback(() => navigate(-1), [navigate])
+  const goBack = useCallback(() => {
+    state?.backToList ? navigate(-1) : navigate(`/package/${packageId}/`)
+  }, [navigate])
 
   const bundleIdToRender = useMemo(() => {
     return bundleId?.length > 30 ? `${bundleId?.slice(0, 20)}...` : bundleId
@@ -29,11 +28,9 @@ const BundleDetailsPage: FC = () => {
         </>
       }
       rightElement={
-        <If condition={state?.backToList && isAuthenticated}>
-          <Button variant="text" onClick={goBack}>
-            Back to the list
-          </Button>
-        </If>
+        <Button variant="text" onClick={goBack}>
+          Back to the list
+        </Button>
       }
     >
       <BundleDetailsBlock {...{ bundleId, packageId }} />
