690f17d62d786d2bf9b0eb944c5b6d8e46a6a691
Serhii Borshchov <s.borshchov.dev@gmail.com>
2024-03-27 11:28:48 +0200

    [content-bundle-widget] widget constructor init

---
+++ src/content-bundle-widget/public/widget-constructor.js
@@ -0,0 +1,35 @@
+const APP_URL = 'http://localhost:5102/'
+
+const createWidgetFrame = (container, frameIndex) => {
+  const { loaded, widgetId } = container.dataset
+  if (loaded) {
+    return
+  }
+  const frame = document.createElement('iframe')
+
+  frame.setAttribute('id', `${widgetId}-${frameIndex}`)
+  frame.setAttribute('src', `${APP_URL}widget/${widgetId}`)
+  frame.setAttribute('height', '325')
+  frame.setAttribute('width', '100%')
+  frame.setAttribute('style', 'border: none;')
+
+  container.appendChild(frame)
+  container.setAttribute('data-loaded', true)
+}
+
+const loadWidgets = () => {
+  const widgetContainers = document.querySelectorAll('[data-type="content-bundle-widget"]')
+  widgetContainers.forEach((container, idx) => createWidgetFrame(container, idx))
+}
+
+loadWidgets()
+
+/* HTML EMBED CODE TEMPLATE
+
+<div
+  data-type="content-bundle-widget"
+  data-widget-id="YOUR WIDGET ID HERE"
+></div>
+<script src="YOUR-APP-HOST-HERE/widget-constructor.js"></script>
+
+*/
