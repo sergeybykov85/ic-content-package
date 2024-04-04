c83cb535971cd2ae001d72e86987b3ace69ccadd
Serhii Borshchov <s.borshchov.dev@gmail.com>
2024-04-04 19:40:38 +0300

    [content-bundle-tools] "Deploy new package" button improvements

--- src/content-bundle-tools/src/components/features/DeployNewPackageBtn/DeployNewPackageBtn.tsx
+++ src/content-bundle-tools/src/components/features/DeployNewPackageBtn/DeployNewPackageBtn.tsx
@@ -1,23 +1,42 @@
-import { type FC, useEffect, useState } from 'react'
+import { type FC, useEffect, useMemo, useState } from 'react'
 import { useServices } from '~/context/ServicesContext'
 import { useAuth } from '~/context/AuthContext'
 import { Link } from 'react-router-dom'
 import Button from '~/components/general/Button'
 
+type Allowance = 'allowed' | 'not-allowed' | 'limit-reached' | 'unknown'
+
 const DeployNewPackageBtn: FC = () => {
   const { principal } = useAuth()
   const { packageService } = useServices()
-  const [allowed, setAllowed] = useState(false)
+  const [allowance, setAllowance] = useState<Allowance>('unknown')
 
   useEffect(() => {
     if (principal && packageService) {
-      packageService?.checkPackageDeployAllowance(principal).then(res => setAllowed(res))
+      packageService?.getActivityBy(principal).then(res => {
+        if (res.allowance === 0) {
+          setAllowance('not-allowed')
+        } else if (res.allowance > res.deployedPackagesNumber) {
+          setAllowance('allowed')
+        } else if (res.allowance === res.deployedPackagesNumber) {
+          setAllowance('limit-reached')
+        }
+      })
     }
   }, [principal, packageService])
 
-  return allowed ? (
+  const tooltip = useMemo(() => {
+    switch (allowance) {
+      case 'not-allowed':
+        return 'You\'re not allowed to deploy packages'
+      case 'limit-reached':
+        return 'You have reached the limit'
+    }
+  }, [allowance])
+
+  return allowance ? (
     <Link to="/deploy-package">
-      <Button text="Deploy new package" />
+      <Button text="Deploy new package" disabled={allowance !== 'allowed'} title={tooltip} />
     </Link>
   ) : null
 }
