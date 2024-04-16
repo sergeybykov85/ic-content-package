bfdd656297f91cd7c772f63703dcd7e3110cf3da
Serhii Borshchov <s.borshchov.dev@gmail.com>
2024-04-16 13:55:48 +0300

    [content-bundle-widget] Delete widget (without confirmation yet)

--- src/content-bundle-widget/src/components/features/WidgetList/WidgetListItem.tsx
+++ src/content-bundle-widget/src/components/features/WidgetList/WidgetListItem.tsx
@@ -1,40 +1,24 @@
 import styles from './WidgetList.module.scss'
-import { type FC, useCallback, useState } from 'react'
+import { type FC, useCallback, useMemo } from 'react'
 import type Widget from '~/models/Widget.ts'
-import shortenPrincipal from '~/utils/shortenPrincipal.ts'
 import clsx from 'clsx'
 import { WIDGET_STATUSES } from '~/types/widgetTypes.ts'
 import If from '~/components/general/If.tsx'
 import Button from '~/components/general/Button'
 import { Link } from 'react-router-dom'
-import { useServices } from '~/context/ServicesContext'
-import { useFullScreenLoading } from '~/context/FullScreenLoadingContext'
 import { enqueueSnackbar } from 'notistack'
 import getWidgetEmbedCode from '~/utils/getWidgetEmbedCode.ts'
 import copyToClipboard from '~/utils/copyToClipboard.ts'
+import IconButton from '~/components/general/IconButton'
 
 interface WidgetListProps {
   widget: Widget
+  onClickActivate: (widgetId: string) => void
+  onClickDelete: (widgetId: string) => void
 }
 
-const WidgetListItem: FC<WidgetListProps> = ({ widget }) => {
-  const { widgetService } = useServices()
-  const { setLoading } = useFullScreenLoading()
-  const [isActive, setIsActive] = useState(widget.status === WIDGET_STATUSES.Active)
-
-  const activateWidget = useCallback(async () => {
-    try {
-      setLoading(true)
-      await widgetService.updateWidget(widget.id, { status: WIDGET_STATUSES.Active })
-      setIsActive(true)
-      enqueueSnackbar('Widget successfully activated', { variant: 'success' })
-    } catch (error) {
-      console.error(error)
-      enqueueSnackbar('Widget activation failed', { variant: 'error' })
-    } finally {
-      setLoading(false)
-    }
-  }, [setLoading, widget.id, widgetService])
+const WidgetListItem: FC<WidgetListProps> = ({ widget, onClickActivate, onClickDelete }) => {
+  const isActive = useMemo(() => widget.status === WIDGET_STATUSES.Active, [widget.status])
 
   const copyWidgetCode = useCallback(() => {
     const embedCode = getWidgetEmbedCode(widget.id)
@@ -45,7 +29,10 @@ const WidgetListItem: FC<WidgetListProps> = ({ widget }) => {
 
   return (
     <div className={styles.item}>
-      <h2 className={styles.title}>{widget.name}</h2>
+      <h2 className={styles.title}>
+        {widget.name}
+        <IconButton iconName="trash.svg" onClick={() => onClickDelete(widget.id)} size={30} className={styles.delete} />
+      </h2>
       <p className={styles.description}>{widget.description}</p>
       <p className={clsx(styles.detail, isActive && styles.active)}>
         <span>Status:</span>
@@ -56,16 +43,12 @@ const WidgetListItem: FC<WidgetListProps> = ({ widget }) => {
         {widget.type}
       </p>
       <p className={styles.detail}>
-        <span>Creator:</span>
-        {shortenPrincipal(widget.creator)}
-      </p>
-      <p className={styles.detail}>
         <span>Created:</span>
         {widget.created}
       </p>
       <div className={styles['btn-group']}>
         <If condition={!isActive}>
-          <Button text="Activate" onClick={activateWidget} />
+          <Button text="Activate" onClick={() => onClickActivate(widget.id)} />
         </If>
         <If condition={isActive}>
           <Button text="Copy embed code" variant="outlined" onClick={copyWidgetCode} />
